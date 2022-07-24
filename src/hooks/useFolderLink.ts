b02e36a1910bebfea3fa56269944f1fe13e62ea5
fcurrk <48197714+fcurrk@users.noreply.github.com>
2022-07-24 16:32:47 +0800

    update 2.6.3

--- src/hooks/useFolderLink.ts
+++ src/hooks/useFolderLink.ts
@@ -7,16 +7,29 @@ const useFolderLink = (proxy = false) => {
   let { pathname } = useLocation();
   let host = import.meta.env.VITE_SERVER_URL as string;
   if (host === "/") {
-    host = window.location.origin + "/";
+    host = window.location.origin;
   }
   let path = pathname.endsWith("/") ? pathname.slice(0, -1) : pathname;
   if (type === "file") {
     path = path.substring(0, path.lastIndexOf("/"));
   }
-  if (proxy) {
-    return `${host}p${path}`;
+  let subfolder = "";
+  if (window.ALIST.subfolder) {
+    subfolder = window.ALIST.subfolder;
   }
-  return `${host}d${path}`;
+  if (subfolder.endsWith("/")) {
+    subfolder = subfolder.substring(0, subfolder.length - 1);
+  }
+  if (!subfolder.startsWith("/")) {
+    subfolder = "/" + subfolder;
+  }
+  if (subfolder === "/") {
+    subfolder = "";
+  }
+  if (proxy) {
+    return `${host}${subfolder}/p${path}`;
+  }
+  return `${host}${subfolder}/d${path}`;
 };
 
 export default useFolderLink;
